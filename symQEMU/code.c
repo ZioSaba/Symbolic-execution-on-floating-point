2ba75e7bad56395a4c370631cb92f4e0253d2663
ZioSaba <sabatini.1794627@studenti.uniroma1.it>
2021-04-23 16:05:58 +0200

    first example of symQEMU finally working

--- symQEMU/code.c
+++ symQEMU/code.c
@@ -1,24 +1,45 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
 #include <unistd.h>
+#include <string.h>
 
-int foo(int a){
-    if (a == 10)
+int foo(char* buf) {
+    
+    char* root = "root";
+    char* user = "user";
+
+    if (!memcmp(buf, root, strlen(root))){
+        printf("Hello, superuser\n");
         return 0;
-    else if (a > 10)
-        return 1;
-    else
-        return -1;
+    }
 
+    else if (!memcmp(buf, user, strlen(user))){
+        printf("Hello, user\n");
+        return 1;
+    }
+
+    else {
+        printf("Hello, %s", buf);
+        return 2;
+    }
+    
+;
 }
 
-
-int main(int argc, char* argv[]){
-    int x;
-    if (read(STDIN_FILENO, &x, sizeof(x)) != sizeof(x)){
-        printf("ERRORE\n");
-        return -1;
+int main() {
+    char* buf = malloc(sizeof(char)*10);
+    
+    if (fgets(buf, 10, stdin) == NULL){
+        printf("Error, aborting...\n");
+        return 1;
     }
-    printf("%d\n", foo(x));
+
+    //printf("%s", buf);
+    //printf("%ld\n", strlen(buf)-1);
+
+    printf("%d\n", foo(buf));
+    
+    free(buf);
     return 0;
 }
