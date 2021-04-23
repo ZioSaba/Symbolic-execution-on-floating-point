93f727e43e4ed05ce295285a0d198b4d5a1d7138
ZioSaba <sabatini.1794627@studenti.uniroma1.it>
2021-04-23 12:02:16 +0200

    testing

--- symQEMU/code.c
+++ symQEMU/code.c
@@ -2,7 +2,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int foo(int a){
+    if (a == 10)
+        return 0;
+    else if (a > 10)
+        return 1;
+    else
+        return -1;
+
+}
+
+
 int main(int argc, char* argv[]){
-    printf("ciao\n");
+    int x;
+    if (read(STDIN_FILENO, &x, sizeof(x)) != sizeof(x)){
+        printf("ERRORE\n");
+        return -1;
+    }
+    printf("%d\n", foo(x));
     return 0;
 }
