b6b4ab830b135aa2e714e59e8af8c6454fe577aa
ZioSaba <sabatini.1794627@studenti.uniroma1.it>
2021-05-28 14:24:05 +0200

    final final example

--- symQEMU/file_input/code_file_input.c
+++ symQEMU/file_input/code_file_input.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
 float foo (int a){
     float x = 15.5;
@@ -14,22 +15,22 @@ float foo (int a){
 
 int main (){
 
-    int desc = open("./input/input.dat", O_RDONLY);
+    int desc = open("/home/zio-saba/Scrivania/progetto_laurea/symQEMU/file_input/input/input.dat", O_RDONLY);
     if (desc == -1){
         printf("Errore nell'apertura del file...\n");
         return -1;
     }
 
-    char buf[4];
+    int buf;
 
-    int byte_letti = read(desc, buf, 4);
+    int byte_letti = read(desc, &buf, 4);
     if (byte_letti < 0){
         printf("Errore nella read...\n");
     }
-    
-    int a = atoi(buf);
-    
-    printf("%f\n", foo(a));
+
+    //int a = atoi(buf);
+    printf("buf=%d\n", buf);
+    printf("%f\n", foo(buf));
 
     if (close(desc) == -1){
         printf("Errore nella chiusura del file...\n");
