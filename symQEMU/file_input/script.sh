6b65c3c68cf6795859a73b29c06adad180cb14e2
ZioSaba <sabatini.1794627@studenti.uniroma1.it>
2021-05-28 10:30:25 +0200

    new final example

--- symQEMU/file_input/script.sh
+++ symQEMU/file_input/script.sh
@@ -4,4 +4,4 @@ export SYMCC_OUTPUT_DIR=/home/zio-saba/Scrivania/progetto_laurea/symQEMU/file_in
 
 export SYMCC_INPUT_FILE=/home/zio-saba/Scrivania/progetto_laurea/symQEMU/file_input/input/input.dat
 
-/home/zio-saba/Scrivania/symqemu_originale/BUILD/x86_64-linux-user/symqemu-x86_64 -d in_asm,op_opt ./test_file
+/home/zio-saba/Scrivania/symqemu/BUILD/x86_64-linux-user/symqemu-x86_64 -d in_asm,op_opt ./test_file
