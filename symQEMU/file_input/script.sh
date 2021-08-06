a5d6d5408cbcedb49defe77b937b59affc12e675
ZioSaba <sabatini.1794627@studenti.uniroma1.it>
2021-08-06 13:44:34 +0200

    hotfix 4.0

--- symQEMU/file_input/script.sh
+++ symQEMU/file_input/script.sh
@@ -6,5 +6,5 @@ export SYMCC_INPUT_FILE=/home/zio-saba/Scrivania/progetto_laurea/symQEMU/file_in
 
 #export SYMCC_INPUT_FILE=/home/zio-saba/Scrivania/progetto_laurea/symQEMU/file_input/risultati/000000
 
-/home/zio-saba/Scrivania/symqemu/BUILD/x86_64-linux-user/symqemu-x86_64 -d in_asm,op_opt ./test_file /home/zio-saba/Scrivania/progetto_laurea/symQEMU/file_input/input/input.dat
-#/home/zio-saba/Scrivania/symqemu/BUILD/x86_64-linux-user/symqemu-x86_64 ./test_file /home/zio-saba/Scrivania/progetto_laurea/symQEMU/file_input/input/input.dat
+#/home/zio-saba/Scrivania/symqemu/BUILD/x86_64-linux-user/symqemu-x86_64 -d in_asm,op_opt ./test_file /home/zio-saba/Scrivania/progetto_laurea/symQEMU/file_input/input/input.dat
+/home/zio-saba/Scrivania/symqemu/BUILD/x86_64-linux-user/symqemu-x86_64 ./test_file /home/zio-saba/Scrivania/progetto_laurea/symQEMU/file_input/input/input.dat
